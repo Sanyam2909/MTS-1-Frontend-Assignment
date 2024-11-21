ee780b3c006140ae4be2c7774d3f1498f275a858
647594 <sanyam.jain@discounttire.com>
2024-11-21 17:28:44 +0530

    Fix link issue3

--- app/components/Logout.tsx
+++ app/components/Logout.tsx
@@ -5,23 +5,23 @@ import { useAuth0 } from '@auth0/auth0-react';
 const Logout = () => {
   const { logout } = useAuth0();
 
-  const handleLogout = async () => {
+  const handleLogout = () => {
     try {
-      // Perform the logout without any arguments
-      logout();
-
-      // Ensure that after logging out, the user is redirected correctly
       const redirectUrl =
         process.env.NODE_ENV === 'production'
           ? 'https://react-trade-dashboard-due5.vercel.app' // Production URL
-          : 'http://localhost:3000'; // Localhost URL for development
+          : 'http://localhost:3000'; // Localhost URL for development;
 
-      // Manually redirect the user after logout
-      window.location.href = redirectUrl;
+      // Use the `returnTo` property for the logout function
+      logout({
+        logoutParams: { returnTo: redirectUrl },
+      });
     } catch (error) {
       console.error('Logout failed:', error);
     }
   };
+
+  console.log('NODE_ENV:', process.env.NODE_ENV);
 
   return (
     <button
